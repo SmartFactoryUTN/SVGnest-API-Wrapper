b15e389849725cfca439a4ce9ace9bb25df47b0b
42Flo <42flo@tuta.io>
2023-07-06 15:13:40 +0200

    added form data in post request to pass svgs in body.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import fs from 'fs';
+import multer from 'multer';
 
 const app = express();
 app.use(cors());
 const port = 3000;
+
+const downloadPath = path.join(process.cwd(), 'downloads');
+const tmpPath = path.join(process.cwd(), 'tmp');
 
 function sleep(ms) {
   return (new Promise((resolve) => {
@@ -16,19 +20,27 @@ function sleep(ms) {
 
 //TODO remove images, fonts, css, everything not needed for nesting
 
-//TODO change to POST, file and bin in request body, optional config, optional iteration count
-app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch({headless: 'new'});
+//TODO add to request: optional config, optional iteration count
+app.post("/", multer().fields([{name: 'bin'}, {name: 'parts'}]), async (req, res) => {
+  // temporary directory to write bin and parts in order to send path to uploadFile(): TODO find a cleaner way to do it
+  fs.mkdirSync(tmpPath, { recursive: true });
+  fs.writeFileSync(path.join(tmpPath, 'bin.svg'), req.files['bin'][0].buffer);
+  fs.writeFileSync(path.join(tmpPath, 'parts.svg'), req.files['parts'][0].buffer);
+
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('http://localhost:8000');
 
-  const filePath = path.relative(process.cwd(), 'parts.svg');
-  const input = await page.waitForSelector('#fileinput');
-  await input.uploadFile(filePath);
+  const partsPath = path.join(tmpPath, 'parts.svg');
+  const partsInput = await page.waitForSelector('#fileinput');
+  await partsInput.uploadFile(partsPath);
 
-  const binPath = path.relative(process.cwd(), 'bin.svg');
+  // const binPath = path.relative(process.cwd(), 'bin.svg');
+  const binPath = path.join(tmpPath, 'bin.svg');
   const binInput = await page.waitForSelector('#bininput');
   await binInput.uploadFile(binPath);
+
+  fs.rmSync(tmpPath, { recursive: true, force: true});
 
   const startButton = await page.waitForSelector('#start');
   await startButton.click();
@@ -47,10 +59,9 @@ app.post("/uploadSvg", express.text(), (req, res) => {
   console.log('output: ', req.body);
   const svgContent = req.body;
   const filename = `svg_${Date.now()}.svg`;
-  const dirPath = path.join(process.cwd(), 'downloads');
-  const filePath = path.join(dirPath, filename);
+  const filePath = path.join(downloadPath, filename);
 
-  fs.mkdirSync(dirPath, { recursive: true });
+  fs.mkdirSync(downloadPath, { recursive: true });
   fs.writeFile(filePath, svgContent, 'utf-8', (err) => {
     if (err) {
       console.error(err);
